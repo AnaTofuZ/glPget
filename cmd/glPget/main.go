af172b06a1bf6821b01b0d3f8592bb365dfcc405
AnaTofuZ <anatofuz@gmail.com>
2017-09-08 19:14:42 +0900

    mainの処理を単純化

--- cmd/glPget/main.go
+++ cmd/glPget/main.go
@@ -1,22 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/AnaTofuZ/glPget"
 )
 
 func main() {
-	cli := glPget.New()
-
-	if err := cli.Run(); err != nil {
-		if cli.Trace {
-			fmt.Fprintf(os.Stderr, "Error:\n%+v\n", err)
-		} else {
-			fmt.Fprintf(os.Stderr, "Error:\n %v\n", err)
-		}
-		os.Exit(1)
-	}
-	os.Exit(0)
+	os.Exit(glPget.New().Run())
 }

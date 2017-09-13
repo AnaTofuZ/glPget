2e215b4af8565270f0eeaa4f3ce8991561b5796a
AnaTofuZ <anatofuz@gmail.com>
2017-09-13 17:15:42 +0900

    http.clientとheadを使う用に修正

--- request.go
+++ request.go
@@ -4,22 +4,17 @@ package glPget
 // 構造体に結果をいれておく
 
 import (
-	context "context"
 	"net/http"
-	"time"
 
 	"github.com/pkg/errors"
-	"golang.org/x/net/context/ctxhttp"
 )
 
 const timeout = 10
 
 func (glp *glPget) CheckSetupURL() error {
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
-	defer cancel()
-
-	res, err := ctxhttp.Head(ctx, http.DefaultClient, glp.URL)
+	client := &http.Client{}
+	res, err := client.Head(glp.URL)
 
 	if err != nil {
 		return errors.New("can't http requests")
